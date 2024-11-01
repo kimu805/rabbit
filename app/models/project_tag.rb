203c2b3bcb7d9489cb6ad9addc83089555ab3560
kimu805 <kimukimuok805@gmail.com>
2024-11-01 18:40:07 +0900

    Update project_tag.rb

--- app/models/project_tag.rb
+++ app/models/project_tag.rb
@@ -3,13 +3,10 @@ class ProjectTag
   attr_accessor :title, :description, :display, :user_id
 
   # validation
-  validates :title, presence: true, length: { maximum: 50 }
-  
-  validates :display, presence: true, inclusion
-
   with_options presence: true do 
     validates :title, length: { maximum: 50 }
     validates :display, inclusion: { in: [0,1,2], message: "は0、1、2、のいずれかである必要があります。"}
   end
   validates :description, length: { maximum: 500 }
+  
 end
