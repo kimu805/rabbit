8395e6537603c27e95870dfa4a308c82cbdfa096
kimu805 <kimukimuok805@gmail.com>
2024-11-01 16:26:35 +0900

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,34 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :show, :destroy]
+
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
+  def update
+    if @project.update(project_params)
+      redirect_to project_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
+  end
+
+  def destroy
+    @project.destroy
   end
 end
