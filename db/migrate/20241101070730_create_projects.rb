24ebe8790747f8a640aad762315a144da04dd931
kimu805 <kimukimuok805@gmail.com>
2024-11-01 16:12:47 +0900

    create projects table

--- db/migrate/20241101070730_create_projects.rb
+++ db/migrate/20241101070730_create_projects.rb
@@ -1,7 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-
+      t.string     :title,      null: false
+      t.text       :description
+      t.boolean    :display,    null: false
+      t.references :user,       null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_30_081828) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_01_070730) do
+  create_table "projects", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.boolean "display", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.text "profile"
@@ -25,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_30_081828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
