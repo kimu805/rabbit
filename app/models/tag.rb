88857d20ddd334b0d3e3ae03f0df65e259f3490e
kimu805 <kimukimuok805@gmail.com>
2024-11-01 19:39:50 +0900

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ApplicationRecord
   # association
   has_many :project_tag_relations
   has_many :projects, through: :project_tag_relations
+
+  # validation
+  validates :tag_name, uniqueness: true
 end
