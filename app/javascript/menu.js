75208b58d48f781d71a42ca3d7cc70c3ccc60957
kimu805 <kimukimuok805@gmail.com>
2024-11-02 17:39:01 +0900

    Create menu.js

---
+++ app/javascript/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("turbo:load", () => {
+  const toggle = document.querySelector(".header_navigation_toggle")
+  const menuList = document.querySelector(".header_navigation_menuList")
+
+  toggle.addEventListener("click", () => {
+    
+  })
+})
