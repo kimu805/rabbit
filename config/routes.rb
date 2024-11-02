24827a116f9dba1c3cc005aa0d19f64f4a4f238e
kimu805 <kimukimuok805@gmail.com>
2024-11-02 13:53:38 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :projects do
+    resources :habits, except: [:index, :show]
     collection do
       get "search"
     end
