7cb832f6ee0bfca1560de6383f86a11407158e68
kimu805 <kimukimuok805@gmail.com>
2024-11-02 14:06:02 +0900

    Update habit.rb

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,15 @@
 class Habit < ApplicationRecord
   # association
   belongs_to :project
+
+  # enum
+  enum frequency: {
+    daily: 1,           # 毎日
+    every_2_days: 2,    # 2日に1回
+    every_3_days: 3,    # 3日に1回
+    every_4_days: 4,    # 4日に1回
+    every_5_days: 5,    # 5日に1回
+    every_6_days: 6,    # 6日に1回
+    weekly: 7           # 7日に1回（毎週）
+  }
 end
