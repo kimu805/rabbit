c4891f163cdb79c725ef6edcd88f15ef20d00062
kimu805 <kimukimuok805@gmail.com>
2024-11-02 13:29:42 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # association
+  has_many :projects
+
+  # validation
   validates :nickname, presence: true, length: { maximum: 20 }
   validates :profile, length: { maximum: 1000 }
+
 end
