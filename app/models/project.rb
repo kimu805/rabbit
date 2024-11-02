68d64cb5dd9ec9911088f777b36d517040abbdfa
kimu805 <kimukimuok805@gmail.com>
2024-11-02 09:15:13 +0900

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,6 @@ class Project < ApplicationRecord
   # enum
   enum display: {
     none_display: 0, # 非公開
-    follower_only: 1, # フォロワーにのみ公開
-    all_display: 2 # 公開
+    all_display: 1 # 公開
   }
 end
