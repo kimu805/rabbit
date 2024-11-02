18a6cf2301015b4ff8f27a8a4253d9aa8d5d30f9
kimu805 <kimukimuok805@gmail.com>
2024-11-02 14:08:12 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
   user = User.create!(nickname: "user#{i+1}", email: "user#{i+1}@user.com", password: "111111", password_confirmation: "111111" )
   project = Project.create!(title: "project#{i+1}", description: "description of project#{i+1}", display: 0, user_id: user.id )
   10.times do |n|
-    Habit.create!(name: "habit#{n+1}", content: "content of habit#{n+1}", frequency: )
+    Habit.create!(name: "habit#{n+1}", content: "content of habit#{n+1}", frequency: 1, project_id: project.id)
   end
 end
 
