82aa30b43e9b000e694b66581eb9831b746bf138
kimu805 <kimukimuok805@gmail.com>
2024-11-03 08:41:14 +0900

    Update habits_controller.rb

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,2 +1,20 @@
 class HabitsController < ApplicationController
+  before_action :set_project
+
+  def new
+    @habit = @project.habits.build
+  end
+
+  def create
+    @habit = @project.habits.build(habit_params)
+  end
+
+  private
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def habit_params
+    params.require(:habit).permit(:name, :content, :frequency)
+  end
 end
