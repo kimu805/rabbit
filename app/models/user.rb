cd7834d09bdbfc0936d245d6210ee74f8f0a081e
kimu805 <kimukimuok805@gmail.com>
2024-11-02 13:58:03 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # association
-  has_many :projects
+  has_many :projects, dependent: :destroy
 
   # validation
   validates :nickname, presence: true, length: { maximum: 20 }
