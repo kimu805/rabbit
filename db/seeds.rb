fdcbcb0e5fbfb3034a7214060e297a9b19633fa5
kimu805 <kimukimuok805@gmail.com>
2024-11-02 14:06:11 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 10.times do |i|
   user = User.create!(nickname: "user#{i+1}", email: "user#{i+1}@user.com", password: "111111", password_confirmation: "111111" )
   project = Project.create!(title: "project#{i+1}", description: "description of project#{i+1}", display: 0, user_id: user.id )
+  10.times do |n|
+    Habit.create!(name: "habit#{n+1}", content: "content of habit#{n+1}", frequency: )
+  end
 end
 
 3.times do |i|
