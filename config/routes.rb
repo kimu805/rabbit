fd81294887fdb24e1cf53fc518489158d52a671d
kimu805 <kimukimuok805@gmail.com>
2024-11-02 09:51:48 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "tags#index"
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :projects do
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
       get "search"
     end
   end
+  resources :tags, only: [:index, :show]
 end
