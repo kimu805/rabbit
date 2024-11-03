a935396f9b136e190569774f2cba86b3df638ab0
kimu805 <kimukimuok805@gmail.com>
2024-11-03 18:27:54 +0900

    Update projects.rb

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :project do
-    
+    title {"project"}
+    description {Faker::Lorem.characters(number: rand(1..500))}
+    display {Faker::Lorem.characters(number: rand(0..1))}
+    association :user
   end
 end
