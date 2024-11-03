312ff33aa81b8dafd62f026d856f96b23154705a
kimu805 <kimukimuok805@gmail.com>
2024-11-03 17:31:20 +0900

    Update habits_controller.rb

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -9,6 +9,7 @@ class HabitsController < ApplicationController
   def create
     @habit = @project.habits.build(habit_params)
     if @habit.save
+      create_check_ins(@habit)
       redirect_to @project, notice: "「#{@habit.name}」を作成しました"
     else
       render :new, status: :unprocessable_entity
@@ -49,5 +50,11 @@ class HabitsController < ApplicationController
 
   def set_habit
     @habit = @project.habits.find(params[:id])
+  end
+
+  def create_check_ins(habit)
+    (Date.today..(Date.today + 30)).each do |date|
+      habit.check_ins.create(date: date, status: false)
+    end
   end
 end
