511cd930fe12d2edec1a9d04d8113f09ffedf589
kimu805 <kimukimuok805@gmail.com>
2024-11-03 18:09:44 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "ユーザー新規登録" do
+    context "上手くいくとき" do
+      it "全ての情報が適切に入力されていれば、登録できる" do 
+        expect(@user).to be_valid
+      end
+      it "profileが空でも登録できる。" do 
+        @user.profile = ""
+        expect(@user).to be_valid
+      end
+    end
+    context "上手くいかないとき" do
+
+    end
+  end
 end
