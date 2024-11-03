3cd76bb50aa8acd21a16528a597a2d8a4bd4eea0
kimu805 <kimukimuok805@gmail.com>
2024-11-03 15:48:02 +0900

    Update check_ins_controller.rb

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,2 +1,10 @@
 class CheckInsController < ApplicationController
+  def update
+    check_in = CheckIn.find_by(id: params[:id])
+    if check_in
+      check_in.status = params[:check_in][:status] == "1"
+      check_in.save
+      redirect_to project_path(params[:project_id])
+    end
+  end
 end
