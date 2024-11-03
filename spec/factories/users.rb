ffd6c7801694a9df217a55a19f797816187035d1
kimu805 <kimukimuok805@gmail.com>
2024-11-03 18:09:37 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname: "test1"
-    email: "test1@test.com"
-    password: "111111"
-    password_confirmation: password
+    nickname {"test1"}
+    profile {Faker::Lorem.characters(number: rand(1..500))}
+    email {"test1@test.com"}
+    password {"111111"}
+    password_confirmation {password}
   end
 end
