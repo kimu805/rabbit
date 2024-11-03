a01477fdfb731594fdef3cf0331dac385ac9c87c
kimu805 <kimukimuok805@gmail.com>
2024-11-03 14:37:13 +0900

    create check_ins table

--- db/migrate/20241103053125_create_check_ins.rb
+++ db/migrate/20241103053125_create_check_ins.rb
@@ -1,7 +1,9 @@
 class CreateCheckIns < ActiveRecord::Migration[7.0]
   def change
     create_table :check_ins do |t|
-
+      t.date :date, null: false
+      t.boolean :status, null: false, default: false
+      t.references :habit, null: false, foreign_key: true
       t.timestamps
     end
   end
