8466ed779da2df81b0c76a9d26512b4e04a26191
kimu805 <kimukimuok805@gmail.com>
2024-11-04 18:59:49 +0900

    Update habit_spec.rb

--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @project = FactoryBot.create(:project)
+    @habit = FactoryBot.build(:habit, project_id: @project.id)
+  end
+  describe "新規作成" do
+    context "上手くいく場合" do
+      it "全ての情報が正しく入力されていれば、作成できる" do
+
+      end
+      it "contentがなくても、作成できる" do
+
+      end
+    end
+    context "上手くいかない場合" do
+      it "nameが空では作成できない" do
+
+      end
+      it "nameが20文字以上では作成できない" do
+
+      end
+      it "contentが500文字以上では作成できない" do
+
+      end
+      it "frequencyが空では作成できない" do
+
+      end
+      it "frequencyは1～7以外の数値は入力できない" do
+
+      end
+    end
+  end
 end
