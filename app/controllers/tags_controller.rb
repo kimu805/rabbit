f9d53fac17d29eb3f9424c1866fcdc5b9d156c29
kimu805 <kimukimuok805@gmail.com>
2024-11-05 18:26:37 +0900

    Update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,5 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @projects = @tag.projects.includes(:owner)
-    add_breadcrumb @tag.tag_name, tag_path(@tag)
   end
 end
