9b58a1bcea57a7748fa249b99647da5975daa409
kimu805 <kimukimuok805@gmail.com>
2024-11-05 13:15:07 +0900

    define association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
   # association
   has_many :projects, dependent: :destroy
+  ## follow
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
 
   # validation
   validates :nickname, presence: true, length: { maximum: 20 }
