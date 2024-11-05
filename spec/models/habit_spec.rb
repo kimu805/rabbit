3aedc59a200be37a7776bf5b4ba708a4d9a5865a
kimu805 <kimukimuok805@gmail.com>
2024-11-05 12:28:18 +0900

    Update habit_spec.rb

--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -8,27 +8,33 @@ RSpec.describe Habit, type: :model do
   describe "新規作成" do
     context "上手くいく場合" do
       it "全ての情報が正しく入力されていれば、作成できる" do
-
+        expect(@habit).to be_valid
       end
       it "contentがなくても、作成できる" do
-
+        @habit.content = ""
+        expect(@habit).to be_valid
       end
     end
     context "上手くいかない場合" do
       it "nameが空では作成できない" do
-
+        @habit.name = ""
+        @habit.valid?
+        expect(@habit.errors.full_messages).to include("習慣名を入力してください")
       end
       it "nameが20文字以上では作成できない" do
-
+        @habit.name = Faker::Lorem.characters(number: 21)
+        @habit.valid?
+        expect(@habit.errors.full_messages).to include("習慣名は20文字以内で入力してください")
       end
       it "contentが500文字以上では作成できない" do
-
+        @habit.content = Faker::Lorem.characters(number: 501)
+        @habit.valid?
+        expect(@habit.errors.full_messages).to include("内容は500文字以内で入力してください")
       end
       it "frequencyが空では作成できない" do
-
-      end
-      it "frequencyは1～7以外の数値は入力できない" do
-
+        @habit.frequency = nil
+        @habit.valid?
+        expect(@habit.errors.full_messages).to include("実行頻度を入力してください")
       end
     end
   end
