e8533c1771a0d99258273fd74a3384b8a054acc1
kimu805 <kimukimuok805@gmail.com>
2024-11-05 13:57:07 +0900

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,15 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    follower = current_user.active_relationships.new(follower_id: params[:user_id])
+    follower.save
+    redirect_to user_path(params[:user_id])
+  end
+
+  def destroy
+    follower = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follower.destroy
+    redirect_to user_path(params[:user_id])
+  end
 end
