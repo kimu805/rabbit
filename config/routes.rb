34616ade792f497ca7f91fc98e5fe535db0a29cb
kimu805 <kimukimuok805@gmail.com>
2024-11-05 13:19:51 +0900

    set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "tags#index"
   devise_for :users
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update, :show] do
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :projects, except: :index do
     resources :habits, except: :index do
       resources :check_ins, only: :update
