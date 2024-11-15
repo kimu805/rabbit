016919f7df04fc559c8edadd5c338a9a3b4c278c
kimu805 <kimukimuok805@gmail.com>
2024-11-15 17:13:44 +0900

    Update follow_switch.js

---
+++ app/javascript/follow_switch.js
@@ -0,0 +1,10 @@
+function follow_switch() {
+  const followingSwitch = document.getElementById("following_switch")
+  const followerSwitch = document.getElementById("follower_switch")
+
+  if (!followingSwitch || !followerSwitch) return null
+  
+}
+
+
+window.addEventListener("turbo:load", follow_switch)
