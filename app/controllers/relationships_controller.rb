f3b08ecfdb484ac867e807597765774465f8f44b
kimu805 <kimukimuok805@gmail.com>
2024-11-15 19:44:40 +0900

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,23 +1,28 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   before_action :except_myself
+  before_action :set_user
 
   def create
-    follower = current_user.active_relationships.new(follower_id: params[:user_id])
+    follower = current_user.active_relationships.new(follower_id: @user.id)
     follower.save
-    redirect_to user_path(params[:user_id])
+    render partial: "relationships/follow_btn", locals: { user: @user }
   end
 
   def destroy
-    follower = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follower = current_user.active_relationships.find_by(follower_id: @user.id)
     follower.destroy
-    redirect_to user_path(params[:user_id])
+    render partial: "relationships/follow_btn", locals: { user: @user }
   end
 
   private
   def except_myself
-    if current_user.id == params[:user_id]
+    if current_user == @user
       redirect_to user_path(current_user.id), alert: "自分自身をフォローすることはできません"
     end
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
