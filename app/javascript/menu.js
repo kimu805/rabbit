c06fd814317c2ccb6376219973c680ac22f1b78b
kimu805 <kimukimuok805@gmail.com>
2024-11-16 17:20:34 +0900

    Update menu.js

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -2,16 +2,17 @@ function menu() {
   const toggle = document.getElementById("toggle")
   const menuList = document.getElementById("menu_list")
 
+  menuList.classList.remove("active")
+
   toggle.addEventListener("click", () => {
     menuList.classList.toggle("active")
   })
 
   window.addEventListener("click", (event) => {
-    if (event.target != toggle && event.target != menuList ) {
+    if (event.target != toggle && !menuList.contains(event.target) ) {
       menuList.classList.remove("active")
     }
   })
 }
 
 window.addEventListener("turbo:load", menu)
-window.addEventListener("turbo:render", menu)
