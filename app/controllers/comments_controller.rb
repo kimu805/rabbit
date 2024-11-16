3e3144743306b79edce11223e1a390b13eeea581
kimu805 <kimukimuok805@gmail.com>
2024-11-16 19:27:49 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_project
+  
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to @project, notice: "コメントを投稿しました"
+    else
+      render "projects/show", alert: "このコメントは投稿できません"
+    end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(project_id: @project.id, id: params[:id])
+    @comment.destroy
+    redirect_to @project, notice: "コメントを削除しました"
+  end
+
+  private
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(project_id: @project.id)
+  end
 end
