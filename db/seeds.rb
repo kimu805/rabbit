d8635bf91f9e8872cbda5fc904d37b27321db89b
kimu805 <kimukimuok805@gmail.com>
2024-11-18 19:03:50 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,3 @@
-# 10.times do |i|
-#   user = User.create!(nickname: "user#{i+1}", email: "user#{i+1}@user.com", password: "111111", password_confirmation: "111111" )
-#   project = Project.create!(title: "project#{i+1}", description: "description of project#{i+1}", display: 1, user_id: user.id )
-#   10.times do |n|
-#     habit = Habit.create!(name: "habit#{n+1}", content: "content of habit#{n+1}", frequency: 1, project_id: project.id)
-#     (Date.today..(Date.today + 30)).each do |date| 
-#       habit.check_ins.create!(date: date, status: false)
-#     end
-#   end
-# end
-
-# 10.times do |i|
-#   project = Project.create!(title: "project#{i+11}", description: "description of project#{i+1}", display: 0, user_id: i+1 )
-#   10.times do |n|
-#     habit = Habit.create!(name: "habit#{n+1}", content: "content of habit#{n+1}", frequency: 1, project_id: project.id)
-#     (Date.today..(Date.today + 30)).each do |date| 
-#       habit.check_ins.create!(date: date, status: false)
-#     end
-#   end
-# end
-
-# tags = [ "Health", "Study", "Bussiness", "Dance" ]
-
-# tags.each do |tag_name|
-#   Tag.create!(tag_name: tag_name)
-# end
-
-# tags_to_assign = Tag.all.sample(1)
-# project.tags << tags_to_assign
-
-# tags_to_assign
-# 
-#
 tags = [ "Health", "Study", "Bussiness", "Dance" ]
 
 tags.each do |tag_name|
