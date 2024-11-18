b22eedc74e87f2ecefb8b4f41ae76166053f913b
kimu805 <kimukimuok805@gmail.com>
2024-11-18 19:03:31 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,8 +49,7 @@ end
 end
 
 10.times do |i|
-  user = User.create!(nickname: "ユーザー#{i+11}", profile:"ユーザー#{i+11}のプロフィールです。", email: "user#{i+11}@user.com", password: "111111", password_confirmation: "111111")
-  project = Project.create!(title: "プロジェクト#{i+11}", description: "プロジェクト#{i+11}の概要です。", display: 0, user_id: user.id)
+  project = Project.create!(title: "プロジェクト#{i+11}", description: "プロジェクト#{i+11}の概要です。", display: 0, user_id: i+1)
   5.times do |n|
     habit = Habit.create!(name: "習慣#{n+1}", content: "習慣#{n+1}の内容です。", frequency: 1, project_id: project.id)
     (Date.today..(Date.today + 30)).each do |date| 
