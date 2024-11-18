cf6ae0a4f9fe0fd0a4225e92d31a73c306142c31
kimu805 <kimukimuok805@gmail.com>
2024-11-18 13:21:55 +0900

    Update comment_channel.js

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -2,18 +2,35 @@ import consumer from "channels/consumer"
 
 if (location.pathname.match(/\/projects\/\d/)) {
   console.log("読み込み完了")
+
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      const html = `
+        <div class= "projectShow_comment">
+          <div class= "projectShow_comment_header_box">
+            <div class= "projectShow_comment_user">${ data.user.nickname }</div>
+            <div class= "projectShow_comment_createdAt">${ data.created_at }</div>
+          </div>
+          <div class= "projectShow_comment_text">${ data.comment.text }</div>
+          <div class= "projectShow_comment_management_box">
+            <div class= "projectShow_comment_deleteBtn">削除</div>
+          </div>
+        </div>
+      `
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML("beforeend", html)
+      const commentForm = document.getElementById("comment_form")
+      commentForm.reset()
+    }
+  });
 }
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
