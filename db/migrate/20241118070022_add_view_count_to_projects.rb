acc1daee17bca642c402608760117ba6e8e3e10f
kimu805 <kimukimuok805@gmail.com>
2024-11-18 16:01:23 +0900

    Update 20241118070022_add_view_count_to_projects.rb

--- db/migrate/20241118070022_add_view_count_to_projects.rb
+++ db/migrate/20241118070022_add_view_count_to_projects.rb
@@ -1,5 +1,5 @@
 class AddViewCountToProjects < ActiveRecord::Migration[7.0]
   def change
-    add_column :projects, :view_count, :integer
+    add_column :projects, :view_count, :integer, default: 0, null: false
   end
 end
