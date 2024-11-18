8024fbeaacf8de2e871953d96e1df687ea1bc1a8
kimu805 <kimukimuok805@gmail.com>
2024-11-18 14:45:04 +0900

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :habits, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   
   # enum
   enum display: {
