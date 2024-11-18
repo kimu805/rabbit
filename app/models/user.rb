3d726385d7f7ff929056ca59e1d4efabf7d55c02
kimu805 <kimukimuok805@gmail.com>
2024-11-18 18:29:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes
+  has_many :view_counts
   ## follow
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
   has_many :followings, through: :active_relationships, source: :follower
