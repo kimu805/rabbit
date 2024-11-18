54e262d0eced501f6d1483788abfd033e1981cd9
kimu805 <kimukimuok805@gmail.com>
2024-11-18 14:20:07 +0900

    Update check_ins_controller.rb

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,11 +1,23 @@
 class CheckInsController < ApplicationController
+  before_action :before_set
+
   def update
     check_in = CheckIn.find_by(id: params[:id])
     if check_in
       check_in.status = params[:check_in][:status] == "1"
       check_in.save
+
+      achievement_rate = @habit.achievement_rate(@habit.start_date || @habit.created_at.to_date, @habit.end_date || Date.today ).to_f
+      overall_achievement_rate = @project.overall_achievement_rate.to_f
+
       # redirect_to project_path(params[:project_id])
-      render json: { check_in: check_in }
+      render json: { check_in: check_in, achievement_rate: achievement_rate, overall_achievement_rate: overall_achievement_rate }
     end
+  end
+
+  private
+  def before_set
+    @project = Project.find(params[:project_id])
+    @habit = Habit.find(params[:habit_id])
   end
 end
