56055229b186f17afbbf88e44a6aabcf46c8c2a4
kimu805 <kimukimuok805@gmail.com>
2024-11-18 15:16:43 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_project
+
+  def create
+    like = current_user.likes.build(project_id: @project.id)
+    like.save
+    redirect_to @project
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, project_id: @project.id)
+    like.destroy
+    redirect_to @project
+  end
+
+  private
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
 end
