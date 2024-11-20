5a38ba49a290013e983ab424bfb2e6ca49ab0739
kimu805 <kimukimuok805@gmail.com>
2024-11-20 11:00:28 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :profile, :email)
+    params.require(:user).permit(:nickname, :profile, :email, :icon)
   end
 
   def only_myself
