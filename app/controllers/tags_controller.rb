0bfc7f5c5bf83420c48d942070cc674e6c4871ff
kimu805 <kimukimuok805@gmail.com>
2024-11-23 18:46:22 +0900

    Update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,6 +7,6 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @q = @tag.projects.where(display: 1).includes(:owner).ransack(params[:q])
-    @projects = @q.result(distinct: true)
+    @projects = @q.result(distinct: true).more_view(@tag.id)
   end
 end
